73cd41add6dcbdf3a60cc6b8f7d43d80db346858
Mike Lee <62190454+mikelee1313@users.noreply.github.com>
2026-04-29 10:34:01 -0400

    Update Get-UnlicensedOneDriveReport.ps1

--- Get-UnlicensedOneDriveReport.ps1
+++ Get-UnlicensedOneDriveReport.ps1
@@ -47,7 +47,7 @@
 
 .NOTES
     File Name   : Get-UnlicensedOneDriveReport.ps1
-    Author      : Mike Lee
+    Author      : Mike Lee | Mariel Williams
     Date Created: 4/28/26
 
     Required Microsoft Graph App Permissions (Application type):
